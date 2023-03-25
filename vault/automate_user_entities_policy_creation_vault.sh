ce4ee924b85b017e950e1648aa89d88f15692b86
vipul71098 <38537174+vipul71098@users.noreply.github.com>
2023-03-25 19:58:31 +0530

    Update automate_user_entities_policy_creation_vault.sh

--- vault/automate_user_entities_policy_creation_vault.sh
+++ vault/automate_user_entities_policy_creation_vault.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-parent_file="testfile.txt"
-VAULT_TOKEN='<VAULT_TOKEN>'
+USERS_PROJECT_MAP_FILE="testfile.txt"
+VAULT_TOKEN=''
+GITLAB_TOKEN=''
 VAULT_ADDR='http://localhost:8200'
-PRIVATE_TOKEN=''
+GITLAB_DOMAIN='<gitlab_domain>'
 while read user_name user_Project
 do  
 
 gitlab_response=$(curl \
-  --header "X-Vault-Token: $PRIVATE_TOKEN" \
+  --header "PRIVATE-TOKEN: $GITLAB_TOKEN" \
   --request GET \
-  "https://<GITLAB_DOMAIN>/api/v4/users?username=$user_name" \
+  "$GITLAB_DOMAIN/api/v4/users?username=$user_name" \
   2>/dev/null)
 
 if [ "$gitlab_response" == "[]" ];then
-   echo "no user exists in gitlab with username $user_name"
+   echo "\n No user exists in gitlab with username : $user_name \n"
 else
    ####################Create Policy########################################################################
     my_list=()
@@ -21,7 +22,7 @@ else
         do  
         my_list+=("$secret_path")
         done  
-    python3 vault_mutipath_policy_creation.py "${my_list[@]}"
+    python3 policy.py "${my_list[@]}"
     curl --header "X-Vault-Token: $VAULT_TOKEN" --request POST --data @vault-policy.json "$VAULT_ADDR/v1/sys/policy/$user_name-policy"
 
 ########################END OF POLICY CREATION##################################################################
@@ -41,20 +42,19 @@ if [ "$status" != "null" ]; then
     num=$(awk 'BEGIN{srand();print int(rand()*1000000000000)}')
     password=$(echo $chars$num | sed 's/\(.\)/\1\n/g' | sort -R | tr -d '\n' | head -c 12)
     PASSWORD=$user_name@$password
-    json=$(curl --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" "https://<GITLAB_DOMAIN>/api/v4/users?username=$user_name" | jq '.[] | select(.username == "'$user_name'")')
+    json=$(curl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "$GITLAB_DOMAIN/api/v4/users?username=$user_name" | jq '.[] | select(.username == "'$user_name'")')
     user_email=$(echo $json | jq -r '.email')
-    python3 send_mail.py "$user_name" "$PASSWORD" "$user_email"
+    #python3 send_mail.py "$user_name" "$PASSWORD" "$user_email"
     echo "$user_name       $PASSWORD           $user_email" >> password.json
 
     curl --header "X-Vault-Token: $VAULT_TOKEN" \
     --request POST \
     --data '{"password": "'$PASSWORD'"}' \
     $VAULT_ADDR/v1/auth/userpass/users/$user_name
-
-    curl --header "X-Vault-Token: $VAULT_TOKEN" \
-    $VAULT_ADDR/v1/sys/auth | jq -r '.data | .["userpass/"].accessor' > accessor_test.txt
 fi
 
+curl --header "X-Vault-Token: $VAULT_TOKEN" \
+    $VAULT_ADDR/v1/sys/auth | jq -r '.data | .["userpass/"].accessor' > accessor_test.txt
 
 ########################END OF CREATE USER###########################################################################
 
@@ -89,13 +89,15 @@ curl --header "X-Vault-Token: $VAULT_TOKEN" \
    --data @payload-alias.json \
    $VAULT_ADDR/v1/identity/entity-alias | jq -r ".data"
 
-######################END OF ALIAS CREATION############################################  
-fi
+######################END OF ALIAS CREATION############################################ 
+
 rm -rf payload-entity.json
 rm -rf payload-alias.json
 rm -rf entity_id.txt
 rm -rf accessor_test.txt
 rm -rf vault-policy.json
 
-done < "$parent_file"
+fi
 
+
+done < "$USERS_PROJECT_MAP_FILE"
